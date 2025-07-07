f6667f65b95e4a7436849a0faf9bc671d7108b43
Mujtaba-s <sayyedmujtaba9637@gmail.com>
2025-07-07 14:40:50 +0530

    done

---
+++ loop.js
@@ -0,0 +1,30 @@
+// let sun = 0;
+// let i = 1;
+
+// while (i <= 5) {
+
+//     sun =sun +i ;
+//     i++
+// }
+
+// console.log(sun);
+
+// let sum = [];
+// let i = 10;
+// while (i > 0)  {
+//     sum.push(i);
+//     i--
+// }
+// console.log(sum);
+
+// let herocoll = []
+// let farvhero;
+
+// do {
+//     farvhero = prompt('ínter you farvheros (Type "done" to finsh)');
+//     if (farvhero != "done") {
+//         herocoll.push(farvhero);
+//     }
+// } while (farvhero != "done" );
+// console.log(herocoll);
+
---
+++ one.js
@@ -0,0 +1,5 @@
+let cars = ["BMW", "Audi", "Saab"];
+
+const model = (cars[2]);
+
+console.log(model);
